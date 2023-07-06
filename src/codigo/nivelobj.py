9c8bc98ae0ea8d3c002ebee170777f245f994c3a
AgustinMondani <33186967+AgustinMondani@users.noreply.github.com>
2023-07-06 16:13:18 -0300

    Delete src directory

--- src/codigo/nivelobj.py
+++
@@ -1,9 +0,0 @@
-class Nivel():
-    def  __init__(self, player, bloques, enemigos, trampas, fondo):
-        self.player = player
-        self.bloques = bloques
-        self.enemigos = enemigos
-        self.trampas = trampas
-        self.fondo = fondo
-
-    
